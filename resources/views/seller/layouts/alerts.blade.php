ec153853541750868778520bfb1dae0f7552d16b
ravindragiri2913 <goswamirvi@gmail.com>
2025-12-01 16:23:38 +0530

    chore: Delete Laravel project scaffolding files.

--- resources/views/seller/layouts/alerts.blade.php
+++
@@ -1,5 +0,0 @@
-@foreach (['danger', 'warning', 'success', 'info'] as $key)
-    @if(Session::has($key))
-        <p class="flashMsg alert alert-{{ $key }}">{{ Session::get($key) }}</p>
-    @endif
-@endforeach
